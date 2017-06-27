3e6bd367878b363699755bf6ea9ee48da2fa5df6
Amir <amir@sd-84147.dedibox.fr>
2017-06-27 23:18:54 +0200

    debug

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t tn-election-img  .
-docker run -itd --rm -p 0.0.0.0:4000:4000 --name tn-election-app tn-election-img
+docker run -itd  -p 0.0.0.0:4000:4000 --name tn-election-app tn-election-img
 
