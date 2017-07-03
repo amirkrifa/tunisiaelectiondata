f330d4b9a9840fdc4492c5bbfc20c56790790a42
Amir Krifa <amir.krifa@euranova.eu>
2017-07-03 14:07:01 +0200

    update app entry point

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ config(['$routeProvider','$locationProvider','markedProvider',function($routePro
 
  // $locationProvider.html5Mode(true);
  // $locationProvider.hashPrefix = '!';
-    $routeProvider.otherwise({redirectTo: '/stories/par'});
+    $routeProvider.otherwise({redirectTo: '/stories/par/full'});
     markedProvider.setOptions({gfm: true});
 
 }]);
